cb37d88abfa0873aff1e2e885dc39339f0615f7a
Solis9 <112727188+Solis9@users.noreply.github.com>
2022-09-12 19:34:28 +0000

    update

---
+++ k26.py
@@ -0,0 +1,44 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    pick_beeper()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
